9f2ab300666001e6fbda69dc3e18a54c1684b1ea
Sumanth0804 <2320030284@klh.edu.in>
2024-09-09 18:44:58 +0530

    Delete Module_1 Project/Module_1 Project/src directory

--- Module_1 Project/Module_1 Project/src/RenderingMethod.java
+++
@@ -1,4 +0,0 @@
-public interface RenderingMethod {
-    String renderCircle(double radius);
-    String renderSquare(double sideLength);
-}
--- Module_1 Project/Module_1 Project/src/Shape.java
+++
@@ -1,3 +0,0 @@
-public interface Shape {
-    String render(RenderingMethod renderingMethod);
-}
