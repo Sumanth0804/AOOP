232c6edcadfb2e44ec4db5b229c079f615ab1c33
Sumanth0804 <2320030284@klh.edu.in>
2024-09-09 18:14:59 +0530

    Delete 7 directory

--- 7/ContactManager.java
+++
@@ -1,6 +0,0 @@
-public interface ContactManager {
-    void addContact(Contact contact);
-    void removeContact(String phoneNumber);
-    Contact getContact(String phoneNumber);
-    void displayContacts();
-}
